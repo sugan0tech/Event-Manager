1d1245e628f0b0a25088ec82c2c7070fe7e7f38a
sugan0tech <sugankpms@gmail.com>
2023-01-20 15:56:19 +0530

    initial commit

---
+++ src/main/java/in/ac/skcet/EventManager/EventManagerApplication.java
@@ -0,0 +1,13 @@
+package in.ac.skcet.EventManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventManagerApplication.class, args);
+	}
+
+}
