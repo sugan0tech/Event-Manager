c43289b4312585209bd5854d152c0db00c76e961
sugan0tech <sugankpms@gmail.com>
2023-01-22 20:20:58 +0530

    updated staff to handle builder

--- src/main/java/in/ac/skcet/event_manager/models/Staff.java
+++ src/main/java/in/ac/skcet/event_manager/models/Staff.java
@@ -4,8 +4,6 @@ package in.ac.skcet.event_manager.models;
 import lombok.*;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
 
@@ -15,10 +13,8 @@ import java.io.Serializable;
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 public class Staff implements Serializable {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private String staffId;
     private String name;
     private String mail;
