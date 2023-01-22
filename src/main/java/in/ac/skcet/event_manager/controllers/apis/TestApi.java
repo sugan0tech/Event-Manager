22314a444d69284026dc9c972e987ee21fe664bf
sugan0tech <sugankpms@gmail.com>
2023-01-22 16:18:37 +0530

    added sample event testing api

--- src/main/java/in/ac/skcet/event_manager/controllers/apis/TestApi.java
+++ src/main/java/in/ac/skcet/event_manager/controllers/apis/TestApi.java
@@ -1,10 +1,13 @@
 package in.ac.skcet.event_manager.controllers.apis;
 
+import in.ac.skcet.event_manager.models.Event;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("/apis")
@@ -14,5 +17,9 @@ public class TestApi {
     public String testApi(@PathVariable String id){
         log.info(id);
         return id;
+    }
+    @PostMapping("/event")
+    public Event getSampleEvent(){
+        return new Event(1, "HourOfPlacement", new Date(), new Date());
     }
 }
