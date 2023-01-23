326b59c8c96176c11ff8cdb03a0bbb9f95826dde
sugan0tech <sugankpms@gmail.com>
2023-01-23 10:28:23 +0530

    added new event api closes #12

--- src/main/java/in/ac/skcet/event_manager/controllers/apis/EventCRUDController.java
+++ src/main/java/in/ac/skcet/event_manager/controllers/apis/EventCRUDController.java
@@ -1,15 +1,14 @@
 package in.ac.skcet.event_manager.controllers.apis;
 
+import in.ac.skcet.event_manager.commands.EventCmdToEvent;
+import in.ac.skcet.event_manager.commands.EventCommand;
 import in.ac.skcet.event_manager.models.Event;
 import in.ac.skcet.event_manager.models.Student;
 import in.ac.skcet.event_manager.repositories.EventRepository;
 import in.ac.skcet.event_manager.repositories.StudentRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,13 +21,20 @@ public class EventCRUDController {
     EventRepository eventRepository;
     StudentRepository studentRepository;
 
-
     @PostMapping("/get/{studentId}")
     public List<Event> getEvents(@PathVariable String studentId){
         Student stu = studentRepository.findById(studentId).orElse(new Student());
         log.info(stu.toString());
 
         return eventRepository.findAll().stream().filter(event -> !stu.getEvents().contains(event)).collect(Collectors.toList());
+    }
+
+    @PostMapping("/new")
+    public void createEvent(@ModelAttribute EventCommand eventCommand){
+        EventCmdToEvent eventCmdToEvent = new EventCmdToEvent();
+        Event event = eventCmdToEvent.convert(eventCommand);
+        log.info(event.toString());
+        eventRepository.save(event);
     }
 
     @PostMapping("/update/{studentId}/{eventId}")
