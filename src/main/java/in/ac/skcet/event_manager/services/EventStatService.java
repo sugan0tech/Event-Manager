ffa13ca61f29b8e7254876f279d1a10e46529b36
sugan0tech <sugankpms@gmail.com>
2023-01-28 18:37:51 +0530

    added functionality for student stat list

--- src/main/java/in/ac/skcet/event_manager/services/EventStatService.java
+++ src/main/java/in/ac/skcet/event_manager/services/EventStatService.java
@@ -2,13 +2,13 @@ package in.ac.skcet.event_manager.services;
 
 import in.ac.skcet.event_manager.models.Event;
 import in.ac.skcet.event_manager.models.Student;
+import in.ac.skcet.event_manager.models.StudentStat;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -22,9 +22,24 @@ public class EventStatService {
 
         int total = students.size();
         log.info("total " + total);
-        int pending = students.stream().filter(student -> !student.getEvents().contains(event)).collect(Collectors.toList()).size();
+        int pending = (int) students.stream().filter(student -> !student.getEvents().contains(event)).count();
         log.info("pending " + pending);
         return pending;
     }
 
+    public List<StudentStat> getStudentStatusList(Integer eventId, String classCode){
+
+        Event  event = eventService.findById(eventId).orElse(new Event());
+        List<Student> students = new ArrayList<>(studentService.findByClassCode(classCode));
+        List<StudentStat> studentStats = new ArrayList<>();
+        students.forEach(
+                student -> {
+                    if(student.getEvents().contains(event)){
+                        studentStats.add(new StudentStat(student, true));
+                    }else
+                        studentStats.add(new StudentStat(student, false));
+                }
+        );
+        return studentStats;
+    }
 }
