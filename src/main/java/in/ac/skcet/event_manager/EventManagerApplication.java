28aa581e70e6addb86e1d4f232387c765dbd4e2f
sugan0tech <sugankpms@gmail.com>
2023-02-06 00:39:14 +0530

    removed jdbc auto config

--- src/main/java/in/ac/skcet/event_manager/EventManagerApplication.java
+++ src/main/java/in/ac/skcet/event_manager/EventManagerApplication.java
@@ -2,8 +2,9 @@ package in.ac.skcet.event_manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {JdbcTemplateAutoConfiguration.class})
 public class EventManagerApplication {
 
 	public static void main(String[] args) {
