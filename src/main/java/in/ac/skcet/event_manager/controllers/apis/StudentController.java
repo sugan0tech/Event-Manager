84444ee66e2af04bed420307afad58ca5af5da8b
sugan0tech <sugankpms@gmail.com>
2023-02-23 22:45:17 +0530

    added sample api for updateOdForm

--- src/main/java/in/ac/skcet/event_manager/controllers/apis/StudentController.java
+++ src/main/java/in/ac/skcet/event_manager/controllers/apis/StudentController.java
@@ -1,13 +1,11 @@
 package in.ac.skcet.event_manager.controllers.apis;
 
+import in.ac.skcet.event_manager.commands.OnDutyFormCommand;
 import in.ac.skcet.event_manager.exception.studentnotfoundexception;
 import in.ac.skcet.event_manager.models.Event;
 import in.ac.skcet.event_manager.services.StudentService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,6 +24,10 @@ public class StudentController {
     @PostMapping("/update/{studentId}/{eventId}")
     public void updateEventStatus(@PathVariable String studentId, @PathVariable String eventId){
         studentService.updateEvent(studentId, eventId);
+    }
+
+    @PostMapping("/addOd/{studentId}")
+    public void updateOdForm(@ModelAttribute OnDutyFormCommand onDutyFormCommand){
     }
 
 }
