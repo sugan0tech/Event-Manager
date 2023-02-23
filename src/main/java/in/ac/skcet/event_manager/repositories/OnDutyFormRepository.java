8a4582ee53383e319ac9a9abe0fa6f60c3dfd903
sugan0tech <sugankpms@gmail.com>
2023-02-23 15:19:52 +0530

    added onduty entity and service #51

---
+++ src/main/java/in/ac/skcet/event_manager/repositories/OnDutyFormRepository.java
@@ -0,0 +1,7 @@
+package in.ac.skcet.event_manager.repositories;
+
+import in.ac.skcet.event_manager.models.OnDutyForm;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OnDutyFormRepository extends JpaRepository<OnDutyForm, Long> {
+}
