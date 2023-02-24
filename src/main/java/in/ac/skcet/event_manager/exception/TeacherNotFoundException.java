3c55250c785581d7df9f2bdc001a7c3298c91b8e
Selvadon123 <83290639+Selvadon123@users.noreply.github.com>
2023-02-24 15:15:55 +0530

    Event handler For Teacher (#56)

---
+++ src/main/java/in/ac/skcet/event_manager/exception/TeacherNotFoundException.java
@@ -0,0 +1,7 @@
+package in.ac.skcet.event_manager.exception;
+
+public class TeacherNotFoundException extends Exception{
+    public TeacherNotFoundException(String message) {
+        super(message);
+    }
+}
