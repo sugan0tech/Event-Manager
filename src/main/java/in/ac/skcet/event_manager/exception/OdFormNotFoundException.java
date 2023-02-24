7c094e8e5618efe0f402269742413dcc60236bb7
sugan0tech <sugankpms@gmail.com>
2023-02-24 09:46:21 +0530

    created od form not found exception.

---
+++ src/main/java/in/ac/skcet/event_manager/exception/OdFormNotFoundException.java
@@ -0,0 +1,7 @@
+package in.ac.skcet.event_manager.exception;
+
+public class OdFormNotFoundException extends Exception{
+    public OdFormNotFoundException(String message) {
+        super(message);
+    }
+}
