26bfe475cda77cf0551a9cc2b6149e5b00989a54
sugan0tech <sugankpms@gmail.com>
2024-02-11 14:24:41 +0530

    code refactor {timeTableHoursService, BootStrapData, H2 optimisations}

--- src/main/java/in/ac/skcet/event_manager/BootstrapDataProd.java
+++ src/main/java/in/ac/skcet/event_manager/BootstrapDataProd.java
@@ -1,9 +1,13 @@
 package in.ac.skcet.event_manager;
 
 import in.ac.skcet.event_manager.event.EventService;
+import in.ac.skcet.event_manager.exception.TeacherNotFoundException;
 import in.ac.skcet.event_manager.on_duty.OnDutyForm;
 import in.ac.skcet.event_manager.on_duty.OnDutyFormService;
 import in.ac.skcet.event_manager.student.StudentService;
+import in.ac.skcet.event_manager.teacher.Teacher;
+import in.ac.skcet.event_manager.teacher.TeacherRepository;
+import in.ac.skcet.event_manager.teacher.TeacherService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
@@ -30,6 +34,7 @@ public class BootstrapDataProd implements CommandLineRunner {
     private ResourceLoader resourceLoader;
     private OnDutyFormService onDutyFormService;
     private EventService eventService;
+    private TeacherService teacherService;
 
 
     @Override
@@ -42,6 +47,13 @@ public class BootstrapDataProd implements CommandLineRunner {
                 });
             }
         });
+        Teacher teacher = Teacher.builder().staffId("srigirit369").classCode("CSE").mail("srigirit369@gmail.com").mobile("9344953235").build();
+        try {
+            teacherService.findById(teacher.getStaffId());
+        }catch (TeacherNotFoundException e){
+            teacherService.save(teacher);
+            log.error(e.toString());
+        }
     }
 
 }
--- src/main/java/in/ac/skcet/event_manager/time_table/TimeTableHoursService.java
+++ src/main/java/in/ac/skcet/event_manager/time_table/TimeTableHoursService.java
@@ -1,45 +1,59 @@
 package in.ac.skcet.event_manager.time_table;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.time.LocalTime;
 
+@Slf4j
 public class TimeTableHoursService {
     private TimeTableHoursService(){}
     private static final LocalTime[] PERIOD_START_TIMES = {
-            LocalTime.parse("08:45"), // Period 0 start time
-            LocalTime.parse("09:40"), // Period 1 start time
-            LocalTime.parse("10:35"), // Period 2 start time
-            LocalTime.parse("11:00"), // Period 3 start time
-            LocalTime.parse("11:55"), // Period 4 start time
-            LocalTime.parse("12:50"), // Period 5 start time
-            LocalTime.parse("13:50"), // Period 6 start time
-            LocalTime.parse("14:45"), // Period 7 start time
-            LocalTime.parse("15:35")  // Period 8 start time
+            LocalTime.parse("08:45"),
+            // period 1
+            LocalTime.parse("09:40"),
+            // period 2
+            LocalTime.parse("10:35"),
+            // morning break
+            LocalTime.parse("11:00"),
+            // period 3
+            LocalTime.parse("11:55"),
+            // period 4
+            LocalTime.parse("12:50"),
+            // lunch break
+            LocalTime.parse("13:50"),
+            // period 5
+            LocalTime.parse("14:45"),
+            // period 6
+            LocalTime.parse("15:35"),
+            // period 7
+            LocalTime.parse("16:45")
     };
 
-    private static final int[] BREAK_PERIODS = {2, 5}; // Periods that have break
-
-    private static final int LUNCH_START_PERIOD = 5;
-    private static final int LUNCH_END_PERIOD = 6;
+    private static final int[] BREAK_PERIODS = {2, 5}; // Periods that are break
 
     private static final LocalTime DAY_START_TIME = LocalTime.parse("08:45");
     private static final LocalTime DAY_END_TIME = LocalTime.parse("16:45");
 
     public static int getPeriodNumber(LocalTime time) {
+        log.info(time.toString());
+
         // Check if time is within clg hours
         if (time.isBefore(DAY_START_TIME) || time.isAfter(DAY_END_TIME)) {
             return 0; // Return 0 for out of clg hours
         }
 
-        for (int i = 0; i < PERIOD_START_TIMES.length; i++) {
+        for (int i = 0; i < PERIOD_START_TIMES.length - 1; i++) {
             // Check if time is within the period start time and end time
-            if (time.isAfter(PERIOD_START_TIMES[i]) && time.isBefore(getPeriodEndTime(i))) {
+            if (time.isAfter(PERIOD_START_TIMES[i]) && time.isBefore(PERIOD_START_TIMES[i + 1])) {
                 if(isBreakPeriod(i)){
+                    log.info("---- Break period ----");
                     return 0;
                 }
-                    return i; // Return period number starting from 1
+                    return i;
             }
         }
 
+        log.info("Un conventional time");
         return 0; // Return 0 for any other case (should never happen)
     }
 
@@ -52,28 +66,14 @@ public class TimeTableHoursService {
         return false;
     }
 
-    private static boolean isLunchPeriod(int period) {
-        return period >= LUNCH_START_PERIOD && period <= LUNCH_END_PERIOD;
-    }
-
-    private static LocalTime getPeriodEndTime(int period) {
-        if (isBreakPeriod(period) || isLunchPeriod(period)) {
-            return PERIOD_START_TIMES[period];
-        } else if (period == PERIOD_START_TIMES.length - 1) {
-            return DAY_END_TIME;
-        } else {
-            return PERIOD_START_TIMES[period + 1];
-        }
-    }
-
-    public  static int convert(int period){
-        if(isBreakPeriod(period))
+    public  static int convert(int period_index){
+        if(isBreakPeriod(period_index))
             return -1;
-        period++;
-        if(period <= 2)
-            return period;
-        if(period <= 5)
-            return period - 1;
-        return period - 3;
+        period_index++;
+        if(period_index <= 2)
+            return period_index;
+        if(period_index <= 5)
+            return period_index - 1;
+        return period_index - 3;
     }
 }
